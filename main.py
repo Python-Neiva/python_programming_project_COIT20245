9e7742553fb89744d9007c4596c2a43dc177b821
hamzacqu <162655263+hamzacqu@users.noreply.github.com>
2024-05-08 02:16:04 +0000

    Checking

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def main():
         if userInput == "help" or userInput ==  "Display help":
             display_menu()
         elif userInput == "exit" or userInput == "Exit the application":
-            print("Exit the Application Hamza")
+            print("Exit the Application")
             exit()
 
 #write display 2 options
