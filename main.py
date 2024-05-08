8c06645c0025983a598833a746fb1b6ff393b5ba
12261175 <162087893+12261175@users.noreply.github.com>
2024-05-08 01:35:49 +0000

    task2

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def choose_option(userInput):
     if userInput == 1 or userInput == "help" or userInput ==  "Display help":
         display_menu()
     elif userInput == 2 or userInput == "Exit" or userInput == "Exit the application":
-        
+        print("Exit the Application")
         exit()
 
     display_menu
