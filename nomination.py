29d93be6df129f7446f29b7fa0d386ba255d6337
Sebastian Romero <srdesingweb@gmail.com>
2024-05-15 16:23:53 +0000

    Refactor display_menu function for improved readability and alignment. Update API URL to use f-string for dynamic city parameter. Fix bug when user input is only "species". Add task 5 option to display menu. Fix bug where the app exits if the input does not fall in any of the given options. Task 4 completed and working together with the team.

--- nomination.py
+++ nomination.py
@@ -4,8 +4,9 @@ import json
 def gps_coordinate(city):
     url = f"https://nominatim.openstreetmap.org/search?q={city}&format=json"
     response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for non-2xx status codes
     data = response.json()
-
+    
     if data:
         # Extract the latitude and longitude from the first result
         latitude = float(data[0]['lat'])
